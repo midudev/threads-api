6b88edfc3def74b2bc023e0523b368902338ab00
Miguel Ángel Durán <miduga@gmail.com>
2023-07-11 20:16:01 +0200

    Add userName endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,13 +7,22 @@ console.log('Initializing API server on port', port)
 
 const app = new Hono()
 
+app.get('/api/users',
+async (context) => {
+  const userName = context.req.query('userName')
+  if (!userName) return context.text('Missing userName', 400)
+
+  const data = await fetchUserProfile({ userName })
+  return context.json(data)
+})
+
 app.get('/api/users/:userId', async (context) => {
   const userId = context.req.param('userId')
   const data = await fetchUserProfile({ userId })
   return context.json(data)
 })
 
-app.use('*', async (c, next) => {
+app.use('*', async (c) => {
   c.notFound()
 })
 
