75f0c84cd140c6da2bbfab004e5f3719d34438e4
Miguel Ángel Durán <miduga@gmail.com>
2023-07-11 20:11:01 +0200

    Add more info

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono';
 import { fetchUserProfile } from '../lib/fetch'
 
-const port = +(Bun.env.PORT ?? 1234)
+const port = +(Bun.env.PORT ?? 3000)
+
+console.log('Initializing API server on port', port)
 
 const app = new Hono()
 
@@ -9,6 +11,10 @@ app.get('/api/users/:userId', async (context) => {
   const userId = context.req.param('userId')
   const data = await fetchUserProfile({ userId })
   return context.json(data)
+})
+
+app.use('*', async (c, next) => {
+  c.json({ error: 'Not found', status: 404 })
 })
 
 export default {
