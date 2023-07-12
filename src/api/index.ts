73882b5c64fd360c85927e8043ee1a5643a655db
Bryan Santos <bryanjared.dev@gmail.com>
2023-07-11 22:57:21 -0400

    Fix fetch, added new enpoints and new doc ids

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,32 +1,104 @@
 import { Hono } from 'hono';
-import { fetchUserProfile } from '../lib/fetch'
+import { fetchThreadReplies, fetchUserProfile, fetchUserProfileThreads } from '../lib/fetch';
 
-const port = +(Bun.env.PORT ?? 3000)
+const port = +(Bun.env.PORT ?? 3000);
 
-console.log('Initializing API server on port', port)
+console.log('Initializing API server on port', port);
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/api/users',
-async (context) => {
-  const userName = context.req.query('userName')
-  if (!userName) return context.text('Missing userName', 400)
 
-  const data = await fetchUserProfile({ userName })
-  return context.json(data)
-})
 
+// Endpoint to get user profiles based on userName
+app.get('/api/users', async (context) => {
+  try {
+    // Extract the userName query parameter from the request
+    const userName = context.req.query('userName');
+
+    // If the userName is missing, return a "Missing userName" error response with status code 400
+    if (!userName) return context.text('Missing userName', 400);
+
+    // Fetch the user profile using the provided userName
+    const data = await fetchUserProfile({ userName });
+
+    // Return the fetched data as a JSON response
+    return context.json(data);
+  } catch (error) {
+    // If an error occurs, respond with a 500 status code and an "Internal Server Error" message
+    return context.text('Internal Server Error', 500);
+  }
+});
+
+// Endpoint to get a specific user profile based on userId
 app.get('/api/users/:userId', async (context) => {
-  const userId = context.req.param('userId')
-  const data = await fetchUserProfile({ userId })
-  return context.json(data)
-})
+  try {
+    // Extract the userId from the request parameters
+    const userId = context.req.param('userId');
+
+    // If the userName is missing, return a "Missing userId" error response with status code 400
+    if (!userId) return context.text('Missing userId', 400);
+
+    // Fetch the user profile using the provided userId
+    const data = await fetchUserProfile({ userId });
+
+    // Return the fetched data as a JSON response
+    return context.json(data);
+  } catch (error) {
+    // If an error occurs, respond with a 500 status code and an "Internal Server Error" message
+    return context.text('Internal Server Error', 500);
+  }
+});
+
+// Endpoint to get replies from a specific thread
+app.get('/api/threads/:threadId/replies', async (context) => {
+  try {
+    // Extract the threadId from the request parameters
+    const threadId = context.req.param('threadId');
+
+    // If the userName is missing, return a "Missing threadId" error response with status code 400
+    if (!threadId) return context.text('Missing threadId', 400);
+
+    // Fetch the thread replies using the provided threadId
+    const data = await fetchThreadReplies({ threadId });
+
+    // Return the fetched data as a JSON response
+    return context.json(data);
+  } catch (error) {
+    // If an error occurs, respond with a 500 status code and an "Internal Server Error" message
+    return context.text('Internal Server Error', 500);
+  }
+});
+
+
+// Endpoint to get user profile threads
+app.get('/api/users/:userId/threads', async (context) => {
+  try {
+    // Extract the userId from the request parameters
+    const userId = context.req.param('userId');
+
+    // If the userName is missing, return a "Missing userId" error response with status code 400
+    if (!userId) return context.text('Missing userId', 400);
+
+    // Fetch the user profile threads using the provided userId
+    const data = await fetchUserProfileThreads({ userId });
+
+    // Return the fetched data as a JSON response
+    return context.json(data);
+  } catch (error) {
+    // If an error occurs, respond with a 500 status code and an "Internal Server Error" message
+    return context.text('Internal Server Error', 500);
+  }
+});
+
+
+
+
 
 app.use('*', async (c) => {
-  c.notFound()
-})
+  c.notFound();
+});
 
 export default {
   port,
   fetch: app.fetch,
-}
+};
